cfed0d3f2e4b231999f86b4f5c1a6b7a307dab6e
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-07-18 21:15:54 +0300

    add my listings section in the profile page and create listing Item component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react"
 import { getAuth, updateProfile } from "firebase/auth"
+import {
+  collection,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
 import { toast } from "react-toastify"
-import {doc, updateDoc} from "firebase/firestore"
 import {db} from "../firebase"
 import { FcHome } from "react-icons/fc";
+import { useEffect } from "react";
+import ListingItem from "../components/ListingItem";
 
 export default function Profile() {
   const auth = getAuth()
   const navigate = useNavigate()
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email
@@ -47,6 +59,27 @@ export default function Profile() {
       toast.error("could not update profile name")
     }
   }
+  useEffect(() => {
+    async function fetchUserListings() {
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
   return (
     <>
       <section className="flex max-w-6xl mx-auto flex-col items-center justify-center">
@@ -70,14 +103,36 @@ export default function Profile() {
               <p className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer" onClick={onLogOut}>Sign out</p>
             </div>
           </form>
-          <button type="submit" className="w-full bg-blue-600 text-white uppercase px-7 py-2 font-medium text-sm rounded shadow-sm hover:bg-blue-700 transition ease-in-out duration-150 hover:shadow-lg active:bg-blue-800">
-            <Link to="/create-listing" className="flex justify-center items-center">
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+          >
+            <Link
+              to="/create-listing"
+              className="flex justify-center items-center"
+            >
               <FcHome className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2" />
               Sell or Rent your home
             </Link>
           </button>
         </div>
       </section>
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
+            <ul>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </>
   )
 }
