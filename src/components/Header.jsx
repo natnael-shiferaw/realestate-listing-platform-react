a092d3355a0bd5edd3a73440f19c2567b12c5a47
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-07-18 04:10:05 +0300

    create a spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ export default function Header(){
       }
     }
   return(
-    <div className="bg-white shadow-sm border-b sticky top-0 z-50">
+    <div className="bg-white shadow-sm border-b sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 mx-w-6xl mx-auto">
         <div>
           <img src="https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg" alt="logo" className='h-5 cursor-pointer' onClick={()=> navigate("/")} />
