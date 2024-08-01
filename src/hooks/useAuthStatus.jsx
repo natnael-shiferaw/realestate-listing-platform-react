488b11d9f29f3bae54d4cbad8682814b33d33ed7
Natnael Shiferaw <130279239+natnael-shiferaw@users.noreply.github.com>
2024-08-01 16:13:17 +0300

    add comments to useAuthStatus.jsx

--- src/hooks/useAuthStatus.jsx
+++ src/hooks/useAuthStatus.jsx
@@ -1,20 +1,21 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { useEffect, useState } from 'react'
 
+// Custom hook to check the authentication status of the user
 export function useAuthStatus() {
-    const [loggedIn, setLoggedIn] = useState(false)
-    const [checkingStatus, setCheckingStatus] = useState(true)
+    const [loggedIn, setLoggedIn] = useState(false) // State to store if the user is logged in
+    const [checkingStatus, setCheckingStatus] = useState(true) // State to store if the authentication status is being checked
 
     useEffect(()=> {
         const auth = getAuth()
-        onAuthStateChanged(auth, (user) => {
+        onAuthStateChanged(auth, (user) => { // Listener for changes in authentication state
             if(user){
-                setLoggedIn(true);
+                setLoggedIn(true); // Set loggedIn to true if user is authenticated
             }
-            setCheckingStatus(false)
+            setCheckingStatus(false) // Set checkingStatus to false after checking the authentication status
         });
-    }, [])
+    }, []) // Empty dependency array to run the effect only once on mount
   return {
-    loggedIn, checkingStatus
+    loggedIn, checkingStatus // Return the authentication status and the checking status
 }
 }
